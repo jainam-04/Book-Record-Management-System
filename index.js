e83707db82e5b22974f6ebb4f0d22d580d47c6dc
Jainam <jainamrupani04@gmail.com>
2025-03-11 16:13:12 +0530

    Added folder data and created two files

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 8081;
+
 app.use(express.json());
+
 app.get("/", (req, res) => {
       res.status(200).json({
             "message": "Server is up and running",
       });
 });
+
+app.get("*", (req, res) => {
+      res.status(404).json({
+            "message": "This route doesn't exists!",
+      });
+});
+
 app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
 })
