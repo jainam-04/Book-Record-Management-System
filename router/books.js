019479808b4be95bd9fedf732700e79b8d8b059d
Jainam <jainamrupani04@gmail.com>
2025-03-12 15:57:37 +0530

    Created routes for creating the new book and update the book by its id and implemented also

--- router/books.js
+++ router/books.js
@@ -104,4 +104,38 @@ router.post("/", (req, res) => {
       });
 });
 
+/**
+ * Route: /:id
+ * Method: PUT
+ * Description: Update the Book
+ * Access: Public
+ * Parameters: Id
+ */
+
+router.put("/:id", (req, res) => {
+      const { id } = req.params;
+      const { data } = req.body;
+      const book = books.find((each) => each.id === id);
+      if (!book) {
+            return res.status(404).json({
+                  success: false,
+                  message: "Book doesn't exists!",
+            });
+      }
+      const updateBookData = books.map((each) => {
+            if (each.id === id) {
+                  return {
+                        ...each,
+                        ...data
+                  };
+            }
+            return each;
+      });
+      return res.status(200).json({
+            success: true,
+            message: "Book Updated Successfully!",
+            data: updateBookData,
+      });
+});
+
 module.exports = router;
