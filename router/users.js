2fcde06ea77a4555474aea6a874387ae80484888
Jainam <jainamrupani04@gmail.com>
2025-03-12 21:41:33 +0530

    Created route for subscription details for user with their id

--- router/users.js
+++ router/users.js
@@ -129,4 +129,66 @@ router.delete("/:id", (req, res) => {
       });
 });
 
+/**
+ * Route: /subscription-details/:id
+ * Method: GET
+ * Description: Getting the subscription details of the user by their id
+ * Access: Public
+ * Parameters: Id
+ */
+
+router.get("/subscription-details/:id", (req, res) => {
+      const { id } = req.params;
+      const user = users.find((each) => each.id === id);
+      if (!user) {
+            return res.status(404).json({
+                  success: false,
+                  message: "User doesn't exists!",
+            });
+      }
+      const getDateInDays = (data = "") => {
+            let date;
+            if (data === "") {
+                  date = new Date();
+            }
+            else {
+                  date = new Date(data);
+            }
+            let days = Math.floor(date / (1000 * 60 * 60 * 24));
+            return days;
+      };
+      const subscriptionType = (date) => {
+            if (user.subscriptionType === "Basic") {
+                  date = date + 90;
+            }
+            else if (user.subscriptionType === "Standard") {
+                  date = date + 180;
+            }
+            else if (user.subscriptionType === "Premium") {
+                  date = date + 365;
+            }
+            return date;
+      };
+      let returnDate = getDateInDays(user.returnDate);
+      let currentDate = getDateInDays();
+      let subscriptionDate = getDateInDays(user.subscriptionDate);
+      let subscriptionExpiration = subscriptionType(subscriptionDate);
+      // console.log("returnDate ", returnDate);
+      // console.log("currentDate ", currentDate);
+      // console.log("subscriptionDate ", subscriptionDate);
+      // console.log("subscriptionExpiration ", subscriptionExpiration);
+      // date format in JSON is MM/DD/YYYY
+      const data = {
+            ...user,
+            isSubscriptionExpired: subscriptionExpiration < currentDate,
+            daysLeftForExpiration: subscriptionExpiration <= currentDate ? 0 : subscriptionExpiration - currentDate,
+            fine: returnDate < currentDate ? subscriptionExpiration <= currentDate ? 100 : 50 : 0,
+      };
+      return res.status(200).json({
+            success: true,
+            message: "Subscription details for the user",
+            data,
+      });
+});
+
 module.exports = router;
